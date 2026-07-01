30192310b2e5ccc2b9813e89c24e3a77fc2706d7
Bensnow <49338381+bensnow-x@users.noreply.github.com>
2026-07-02 06:58:45 +0700

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "================================="
+echo "      START DAC NODE"
+echo "================================="
+
+echo "Starting node..."
+
+# Ganti dengan command asli nanti
+echo "./gdacnode --config config.toml"
+
+echo
+read -p "Press Enter..."
+bash ../dac-node.sh
