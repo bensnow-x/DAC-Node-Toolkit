4208b147c260194a095eefcebfbc6b5f0eb47c14
Bensnow <49338381+bensnow-x@users.noreply.github.com>
2026-07-02 07:00:03 +0700

    Create logs.sh

---
+++ scripts/logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+echo "================================="
+echo "        LIVE LOGS"
+echo "================================="
+
+echo
+echo "Replace this with:"
+echo "tail -f /path/to/dac.log"
+
+read -p "Press Enter..."
+bash ../dac-node.sh
