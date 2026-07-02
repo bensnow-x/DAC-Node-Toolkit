1796683637e178fadb47466a4a25f59ae90ce5ec
Bensnow <49338381+bensnow-x@users.noreply.github.com>
2026-07-02 12:49:43 +0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,9 +20,6 @@ echo "./gdacnode --config config/config.toml"
 echo
 echo "Done."
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(dirname "$SCRIPT_DIR")"
-
 echo
-read -p "Press Enter to return to menu..."
-exec bash "$ROOT_DIR/dac-node.sh"
+read -p "Press Enter..."
+exit 0
